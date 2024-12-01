3f7582b9483b216a662e31d63e4cb6d6fe95786d
Sole <114538195+mahhanakbaran@users.noreply.github.com>
2024-12-01 11:58:54 -0600

    I want to work on my pc now.

--- main.c
+++ main.c
@@ -2,19 +2,35 @@
 #include <stdlib.h>
 #include <time.h>
 
+
+unsigned int linearCongruentialGenerator(unsigned int seed, unsigned int min, unsigned int max) {
+
+}
 int main(void) {
     printf("Random Number Generator\n");
 
     int numberRange;
     time_t seconds;
     seconds = time(NULL);
-    printf("%d\n",seconds);
-    
 
-    // printf("Random Number Generator starts at %ld\n", seconds);
-    
-    if (seconds == -1) {
-        printf("The time()Function");
+
+    int min, max, seed;
+    printf("Enter min number: ");
+
+    scanf("%u", &min);
+
+    printf("Enter max number: ");
+
+    scanf("%u", &max);
+
+    if ((min < 0) || (max < 0) || (seed < 0)) {
+        printf("Do not enter a negative number.");
     }
+    // printf("Random Number Generator starts at %ld\n", seconds);
+     
+    // if (seconds == -1) {
+    //     printf("The time()Function");
+    // }
+
     return 0;
 }
