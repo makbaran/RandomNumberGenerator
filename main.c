317e0e953acec1dedccaf2925b059544b1df3eae
Sole <114538195+SoleStriker@users.noreply.github.com>
2024-12-01 22:28:14 -0600

    Finished

--- main.c
+++ main.c
@@ -3,34 +3,50 @@
 #include <time.h>
 
 
-unsigned int linearCongruentialGenerator(unsigned int seed, unsigned int min, unsigned int max) {
+int linearCongruentialGenerator(int seed, int min, int max) {
+    int randValue;
+    int current = seed;
+    
+
+    int a = 1103515245;
+    int c = 12345;
+    unsigned int m = 2147483648;
+
+    current = (a * current + c) % m;
+
+    randValue = (current % (max - min + 1)) + min;
+
+    return randValue;
 
 }
+
 int main(void) {
     printf("Random Number Generator\n");
 
-    int numberRange;
     time_t seconds;
     seconds = time(NULL);
 
 
-    int min, max, seed;
+    int min, max;
     printf("Enter min number: ");
 
-    scanf("%u", &min);
+    scanf("%d", &min);
 
     printf("Enter max number: ");
 
-    scanf("%u", &max);
+    scanf("%d", &max);
 
-    if ((min < 0) || (max < 0) || (seed < 0)) {
-        printf("Do not enter a negative number.");
+    if ((min < 0) || (max < 0) || (min >= max)) {
+        printf("Invalid input. Ensure min and max are non-negative and min is less than max.\n");
+        return 1;
     }
-    // printf("Random Number Generator starts at %ld\n", seconds);
-     
-    // if (seconds == -1) {
-    //     printf("The time()Function");
-    // }
+
+    int finalVal = linearCongruentialGenerator(seconds, min, max);
+    
+    printf("%d\n", finalVal);
+    
+
+
 
     return 0;
 }
