d5fbe77f5cef157a6fcca85881348f49998d6236
Sole <114538195+mahhanakbaran@users.noreply.github.com>
2024-12-04 10:48:28 -0600

    added last print statement

--- main.c
+++ main.c
@@ -39,7 +39,7 @@ int main(void) {
 
     int finalVal = linearCongruentialGenerator(seconds, min, max);
 
-    printf("%d\n", finalVal);
+    printf("The random number is: %d\n", finalVal);
 
     return 0;
 }
